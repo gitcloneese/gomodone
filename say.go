c2c63724475d2c8ff649188381f6942372cdd3e9
panliangliang <panll@blizzmi.com>
2021-02-26 01:41:48 +0800

    v2

--- say.go
+++ say.go
@@ -2,8 +2,20 @@ package gomodone
 
 import (
 	"fmt"
+	"errors"
 )
 
-func SayHi(name string) string {
-	return fmt.Sprintf("Hi, %s", name)
+func SayHi(name, lang string) (string, error) {
+	switch lang {
+		case "en" :
+			return fmt.Sprintf("Hi, %s!", name), nil
+		case "pt" :
+			return fmt.Sprintf("Oi, %s!", name), nil
+		case "es" :
+			return fmt.Sprintf("Hola, %s!", name), nil
+		case "fr" :
+			return fmt.Sprintf("Bonjour, %s!", name), nil
+		default :
+			return "", errors.New("unkown language ...")
+	}
 }
